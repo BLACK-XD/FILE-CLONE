c13caf2f910f35c0a105c1d062a60055bd6cbd55
MR COPY PASTER <125338978+BLACK-XD@users.noreply.github.com>
2023-02-14 18:00:33 +0600

    Update BLACK.py

--- BLACK.py
+++ BLACK.py
@@ -1,1 +1,17 @@
-import BLACK
+import os, platform
+
+os.system('git pull')
+
+os.system('clear')
+
+import requests
+
+bit = platform.architecture()[0]
+
+if bit == '64bit':
+
+    import BLACK
+
+elif bit == '32bit':
+
+    import BLACK32
